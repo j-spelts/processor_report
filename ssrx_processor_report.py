425d37f675b6a12a431d2ab49e72d8ae9349cf49
j_spelts <joespelts@gmail.com>
2016-08-26 10:33:05 -0700

    added logic to find all processors necessary. added main method

--- ssrx_processor_report.py
+++ ssrx_processor_report.py
@@ -1,27 +1,33 @@
 import os 
-import csv
-from openpyxl import Workbook
+# import csv
+# from openpyxl import Workbook
 
-class processorReportGenerator():
+class ProcessorReportGenerator():
 
-	SQL= 'select '\
-	'    processor  PROCESSOR'\
-	'    , id_type  PAYMEMT_LEVEL'\
-	'    , payment_id  PAYMENT_LEVEL_CODE'\
-	'    , provider  PAYMENT_DESCRIPTION;'\
-	'from ssrx.eob_835'\
-	'where processor like ''ReconRx%'''\
-	';'\
-	''
+	def main(self,args=None):
+		pass
 
-	DSN= 'SSRXDEVIQ1'
+	PROCESSORS = ('provider pay', 'inmar')
+
+	DSN = 'SSRXDEVIQ1'
 
 	# run sql and output to a csv
-	def runSQL(SQL):
+	def runSQL(self):
 
-		os.system('dbisql -c ' + DSN + ' -nogui "' + SQL + '" output to ~ quote '' delimited by '','';')
-		os.system('echo SQL has succesfully run')
+		for processor in self.PROCESSORS:
+			SQL = 'call ssrx.ssrx_835_report(' + processor + ')'
+			os.system('dbisql -c ' + DSN + ' -nogui "' + SQL + '" output to ~ quote '' delimited by '','';')
+
+			print ('run sql for ' + processor)
+
+		# os.system('echo SQL has succesfully run')
 
 	# convert csv file to .xls
-	def fileConversionXLS():
-		
+	# def fileConversionXLS():
+
+def main(args=None):
+	processor = ProcessorReportGenerator()
+	processor.runSQL()
+
+
+if __name__ == '__main__': main()
